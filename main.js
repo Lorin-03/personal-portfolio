8e20bcccb4d0106c2dab4885028dccf05e599f5d
Lorin <loringailedr.pagdanganan@gmail.com>
2026-07-09 22:45:40 +0800

    Update contact section: add GitHub and LinkedIn icons; remove copy functionality

--- main.js
+++
@@ -1,9 +0,0 @@
-// function copyText(icon) {
-//   const text = icon.previousElementSibling.innerText;
-//   navigator.clipboard.writeText(text).then(() => {
-//     icon.classList.replace("bi-copy", "bi-check"); // changes icon to checkmark
-//     setTimeout(() => {
-//       icon.classList.replace("bi-check", "bi-copy"); // revert back after 1.5s
-//     }, 1500);
-//   });
-// }
